036e0c43b38bdf03415022555fa3920a15893488
BriceM <brice.mangeat@gmail.com>
2019-02-12 17:11:18 +0100

    Update interface of main page

--- convert/ViewController.swift
+++ convert/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var cashView: UIView!
+    @IBOutlet weak var lengthView: UIView!
+    @IBOutlet weak var temperatureView: UIView!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cashView.layer.shadowColor = UIColor.gray.cgColor
+        cashView.layer.shadowOpacity = 0.2
+        cashView.layer.shadowOffset = CGSize(width: -1.0, height: 1.0)
+        cashView.layer.shadowRadius = 4.0
+        
+        lengthView.layer.shadowColor = UIColor.gray.cgColor
+        lengthView.layer.shadowOpacity = 0.2
+        lengthView.layer.shadowOffset = CGSize(width: -1.0, height: 1.0)
+        lengthView.layer.shadowRadius = 4.0
+        
+        temperatureView.layer.shadowColor = UIColor.gray.cgColor
+        temperatureView.layer.shadowOpacity = 0.2
+        temperatureView.layer.shadowOffset = CGSize(width: -1.0, height: 1.0)
+        temperatureView.layer.shadowRadius = 4.0
+        
     }
 
-
+    @IBAction func actionButton(_ sender: UIButton)
+    {
+        
+    }
+    
 }
 
